5df1b91f2cc7fc3758de38da5039d4d45d7b1deb
whx718 <1257334212@qq.com>
2023-02-23 21:52:09 +0800

    week6 done

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,39 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
+
+    for _ in 0..input_vec.len() {
+        output_vec.push(U::default());
+    }
+
     // TODO: implement parallel map!
+    let (sender1, receiver1) = crossbeam_channel::unbounded();
+    let (sender2, receiver2) = crossbeam_channel::unbounded();
+    let mut threads = Vec::new();
+
+    for _ in 0..num_threads {
+        let receiver = receiver1.clone();
+        let sender = sender2.clone();
+
+        threads.push(thread::spawn(move || {
+            while let Ok((i, t)) = receiver.recv() {
+                sender.send((i, f(t))).expect("msg");
+            }
+        }));
+    }
+
+    drop(sender2);
+
+    for (i, t) in input_vec.into_iter().enumerate() {
+        sender1.send((i, t)).expect("msg");
+    }
+
+    drop(sender1);
+
+    while let Ok((i, u)) = receiver2.recv() {
+        output_vec[i] = u;
+    }
+
     output_vec
 }
 
